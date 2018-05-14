d19f5d790ded0a00a443e9a53284686e5508e72b
Stephen Mayhew <mayhew2@illinois.edu>
2018-05-14 16:53:13 -0500

    Add pom to lib

--- ner/install-lbjava-weighted.sh
+++ ner/install-lbjava-weighted.sh
@@ -1,2 +1,4 @@
-mvn install:install-file -Dfile=lib/LBJava-1.3.0-SNAPSHOT.jar -DgroupId=edu.illinois.cs.cogcomp -DartifactId=LBJava -Dversion=1.3.0-SNAPSHOT -Dpackaging=jar
+#mvn install:install-file -Dfile=lib/LBJava-1.3.0-SNAPSHOT.jar -DgroupId=edu.illinois.cs.cogcomp -DartifactId=LBJava -Dversion=1.3.0-SNAPSHOT -Dpackaging=jar
+mvn install:install-file -Dfile=lib/LBJava-1.3.0-SNAPSHOT.jar -DpomFile=lib/LBJava-1.3.0-SNAPSHOT.pom
+
 mvn dependency:copy-dependencies
